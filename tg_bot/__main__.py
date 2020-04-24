5eeb3262ce7391580dbb58a3d5c3eb20aa4850df
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-04-24 07:09:44 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,19 +19,20 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @Huduma.
+Habari yako {}, Mimi ni Robot Jinalangu ni {}! Kupata Menu zote, Gusa hapo 👉 /help - *Karibu👇 group*  \n\n[Huduma 📶](https://t.me/Huduma)
 
 I'm a group manager bot maintained by [this wonderful person](tg://user?id={}). I'm built in python3, using the \
-python-telegram-bot library, and am fully [opensource](https://github.com/paulsonoflars/tgbot) - you can find what makes me tick \
-[here](github.com/aboyhasnooname/tgbot)!
+python-telegram-bot library, and am fully [opensource](https://t.me/twuwbaa).
+[here](https://t.me/twuwbaa)!
 
 Feel free to submit pull requests on github, or to contact my support group, @Huduma, with any bugs, questions \
 or feature requests you might have :)
-I also have a news channel, @HabariTz for announcements on new features, downtime, etc.
+Kwa habari Mbali Mbali itakubidi utembelee channel,👉[Swahili News](https://t.me/HabariTz).\
 
-You can find the list of available commands with /help.
+Tumia command hii /help Kupata Menu zote
 
-If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
+
+If you're enjoying using me, and/or would like to help me survive in the wild, hit /hamis to help fund/upgrade my VPS!
 """
 
 HELP_STRINGS = """
