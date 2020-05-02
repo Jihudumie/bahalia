04dea37b3260935025bb8761bea656f405e0383e
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-05-02 17:41:45 +0300

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -217,7 +217,7 @@ __help__ = """
  - /demote: demotes the user replied to
 """
 
-__mod_name__ = "Admin"
+__mod_name__ = "🅰dmin"
 
 PIN_HANDLER = CommandHandler("pin", pin, pass_args=True, filters=Filters.group)
 UNPIN_HANDLER = CommandHandler("unpin", unpin, filters=Filters.group)
